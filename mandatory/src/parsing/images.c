8360c12fd97981afbc474b6e7119e71488bd1cac
smaccary <smaccary@student.42.fr>
2020-07-29 01:23:04 +0200

    normed images.c

--- mandatory/src/parsing/images.c
+++ mandatory/src/parsing/images.c
@@ -6,7 +6,7 @@
 /*   By: smaccary <smaccary@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/26 16:55:14 by smaccary          #+#    #+#             */
-/*   Updated: 2020/07/28 18:48:32 by smaccary         ###   ########.fr       */
+/*   Updated: 2020/07/29 01:22:31 by smaccary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -123,20 +123,17 @@ int			write_bmp(char *path, t_data *data)
 	void		*ptr;
 	t_texture	text;
 
-
 	ptr = ft_calloc(14, 1);
 	((char *)ptr)[0] = 'B';
 	((char *)ptr)[1] = 'M';
 	image_size = data->width * data->height * data->bits_per_pixel / 8;
 	*(uint32_t *)(ptr + 2) = BMP_HEADER_SIZE + image_size;
 	*(uint32_t *)(ptr + 2 + 8) = 54;
-	bih = (t_bih)
-	{
-		sizeof(t_bih), data->width, -data->height, 1, 32, 0,
-		image_size + BMP_HEADER_SIZE,
-		BMP_DPI * 39.375, BMP_DPI * 39.375, 0, 0
-	};
-	if ((fd = open(path, O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH)) <= 0)
+	bih = (t_bih){
+	sizeof(t_bih), data->width, -data->height, 1, 32, 0,
+	image_size + BMP_HEADER_SIZE, BMP_DPI * 39.375, BMP_DPI * 39.375, 0, 0};
+	if ((fd = open(path, O_WRONLY | O_CREAT | O_TRUNC,
+	S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)) <= 0)
 		return (FILE_INVALID_ERROR);
 	write(fd, (void *)ptr, 14);
 	write(fd, (void *)&bih, sizeof(t_bih));
