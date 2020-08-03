2d2a861e3b543af8a601d1fe8cef62ed90e3af09
Smaccary <smaccary@student.42.fr>
2020-08-03 20:54:57 +0200

    normed

--- bonus/src/events/singletons.c
+++ bonus/src/events/singletons.c
@@ -6,11 +6,12 @@
 /*   By: smaccary <smaccary@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/23 01:09:32 by user42            #+#    #+#             */
-/*   Updated: 2020/08/03 19:31:14 by smaccary         ###   ########.fr       */
+/*   Updated: 2020/08/03 20:48:29 by smaccary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "events.h"
+#include "garbage_collection.h"
 
 int		leave(t_vars *vars)
 {
